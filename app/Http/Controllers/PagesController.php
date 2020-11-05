cdaeaf0ea551467bcf03e5906708a5efedf89d33
Egi <egi.mata1@gmail.com>
2020-11-05 16:57:39 +0100

    First Commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        $title = 'Welcome';
+        return view('pages.index')->with('title', $title);
+    }
+    public function about()
+    {
+        return view('about.about');
+    }
+    public function services()
+    {
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+}
